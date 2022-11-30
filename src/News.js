3086a8fb3bcde7e8b0c24e4fb8cb6942bf479dff
EitasamButt101 <117992543+EitasamButt101@users.noreply.github.com>
2022-11-30 16:47:44 +0500

    first commit

--- src/News.js
+++ src/News.js
@@ -89,20 +89,59 @@ export class News extends Component {
         console.log("Hello i am a constructor from news component")
         this.state={
            articles: this.articles,
-           loading: false            
+           loading: false,
+           page:1          
         }
       }
+      async componentDidMount(){
+        console.log("cdm")
+        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8a216d57d34b433da5b07e4783562b84&page=1"
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({articles: parsedData.articles})
+      }
+  handlePrevClick = async () =>{
+console.log("Previous")
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8a216d57d34b433da5b07e4783562b84&page ${this.state.page -1}`
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState=({
+          page : this.state.page - 1,
+          articles: parsedData.articles
+          
+        })
+      }
+     handleNextClick = async () =>{
+        console.log("Next")
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8a216d57d34b433da5b07e4783562b84&page=${this.state.page + 1}`
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState=({
+          page: this.state.page +1,
+          articles: parsedData.articles
+          
+        })
+      }
+
   render() {
+    console.log("render")
     return (
       <div className="container my-3">
         <h2>WorldNews - Top Headlines</h2>
         <div className="row">
         {this.state.articles.map((element)=>{
            return <div className="col-md-4" key={element.url}>
-        <NewsItem  title={element.title.slice(0, 45)} description={element.description.slice(0, 76)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+        <NewsItem  title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 76):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
         </div>
         })}
         
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled= {this.state.page<=1} type="button" class="btn btn-dark " onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" class="btn btn-dark " onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )

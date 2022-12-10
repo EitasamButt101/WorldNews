b5ad794c79dcdfd8591758e2a44c153d828085c1
EitasamButt101 <117992543+EitasamButt101@users.noreply.github.com>
2022-12-10 11:07:15 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
+import "./App.css";
 
-import './App.css';
-
-import React, { Component } from 'react'
-import Navbar from './Navbar';
-import News from './News';
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import News from "./News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-      <News/>
+        <Router>
+          <Navbar />
+
+          <Routes>
+            <Route
+              exact
+              path="/business"
+              element={<News country="us" category="business" />}
+            >
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  country="us"
+                  category="entertainment"
+                />
+              }
+            >
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/general"
+              element={<News key="general" country="us" category="general" />}
+            >
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/health"
+              element={<News key="health" country="us" category="health" />}
+            >
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/science"
+              element={<News key="science" country="us" category="science" />}
+            >
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/sports"
+              element={<News key="sports" country="us" category="sports" />}
+            >
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News key="technology" country="us" category="technology" />
+              }
+            >
+              {" "}
+            </Route>
+          </Routes>
+        </Router>
       </div>
-    )
+    );
   }
 }
-
